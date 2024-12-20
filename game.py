d6924fc07b1cf0c78a904e8ebb27f122493d5034
tungstencube44burger <zinkslink4286@gmail.com>
2024-12-19 20:23:48 -0500

    Nova Numbers Guessing V1.0
    Bug fixes coming soon- read the README to learn how to play.

---
+++ game.py
@@ -0,0 +1,22 @@
+print ("Welcome to Numbers Guessing")
+
+import random
+
+low = 1
+high = 25
+guesses = 0
+number = random.randint(low,high)
+
+while True:
+    guess = int(input(f"Enter number between ({low} - {high}):"))
+    guesses += 1
+
+    if guess < number:
+        print (f"{guess} is too low")
+    elif guess > number:
+        print (f"{guess} is too high")
+    else:
+        print (f"{guess} is right!")
+        break
+
+    print (f"This round took you {guesses}")g
