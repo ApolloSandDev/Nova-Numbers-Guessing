b3a61cc834ccced663a994182178425473c6871b
tungstencube44burger <zinkslink4286@gmail.com>
2024-12-22 20:58:56 -0500

    Nova Number Guessing (Python Edition) Version 2.0
    Making release notes soon

--- game.py
+++ game.py
@@ -1,26 +1,32 @@
-print ("Welcome to Nova Number Guessing Version 1.1")
+print ("Welcome to Nova Number Guessing Version 2.0")
+githubinfo = ("Go to my Github Repo called 'Nova-Number-Guessing' to view future events and updates")
+print (githubinfo)
+print ("Good Luck!")
+endlog = ("Thank you for playing! Check my Git Repo for more!")
 
-input("Press any key then press enter to begin playing")
+input("Press enter to begin playing")
 
 import random
 
 low = 1
-high = 25
+high = 40
 tries = 0
 number = random.randint(low,high)
 
 while True:
-    guess = int(input(f"Guess the number... It will be between ({low} - {high}):"))
+    guess = int(input(f"Enter your guess... It will be between ({low} - {high}):"))
     tries += 1
 
     if guess < number:
-        print (f"{guess} is too low")
+        print (f"{guess} is too little. Try again!")
     elif guess > number:
-        print (f"{guess} is too high")
+        print (f"{guess} is too much. Try again!")
     else:
-        print (f"{guess} is right!")
+        print (f"{guess} is correct! Good job!")
 
-        print(f"This round took you {tries}")
+        print(f"This round took you {tries} tries")
+        break
 
-    playagain = input (f"Do you want to guess again? If so, press enter")
-    guess = random.randint(low,high)
+print (endlog)
+
+
