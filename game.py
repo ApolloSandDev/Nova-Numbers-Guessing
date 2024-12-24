e9881552d7e9c80b767511e8e48426e6ee9ea645
tungstencube44burger <zinkslink4286@gmail.com>
2024-12-24 11:46:45 -0500

    Nova Number Guessing (Python Edition) Version 2.1
    Read changelog

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-print ("Welcome to Nova Number Guessing Version 2.0")
+print ("Welcome to Nova Number Guessing Version 2.1")
 githubinfo = ("Go to my Github Repo called 'Nova-Number-Guessing' to view future events and updates")
 print (githubinfo)
 print ("Good Luck!")
@@ -24,9 +24,49 @@ while True:
     else:
         print (f"{guess} is correct! Good job!")
 
-        print(f"This round took you {tries} tries")
+        print(f"This round took you {tries} tries to get it right")
+        break
+
+playagainlog = input ("This time, it will be harder! Press enter to defeat the final round!")
+
+low = 1
+high = 70
+tries = 0
+number = random.randint(low,high)
+
+while True:
+    guess = int(input(f"Enter your guess... It will be between ({low} - {high}):"))
+    tries += 1
+
+    if guess < number:
+        print (f"{guess} is too little. Try again!")
+    elif guess > number:
+        print (f"{guess} is too much. Try again!")
+    else:
+        print (f"{guess} is correct! Good job!")
+
+        print(f"This harder round took you {tries} tries to get it right")
+        break
+
+twoplaylog = input ("But wait... That's Not all!!! This time, it will be extremely tough! Press enter to win the extreme round!")
+
+low = 1
+high = 100
+tries = 0
+number = random.randint(low,high)
+
+while True:
+    guess = int(input(f"Enter your guess... It will be between ({low} - {high}):"))
+    tries += 1
+
+    if guess < number:
+        print (f"{guess} is too little. Try again!")
+    elif guess > number:
+        print (f"{guess} is too much. Try again!")
+    else:
+        print (f"{guess} is correct! Good job!")
+
+        print(f"This extreme round took you {tries} tries to get it right! Amazing!")
         break
 
 print (endlog)
-
-
